0b82a0322045591382ab9873006149684c9f8c13
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-06-23 16:08:19 +0530

    Employee Monthly Salary Added git add .

--- dailyWage.sh
+++ dailyWage.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-empRate=20;
-fullTime=8;
+partTime=1
+fullTime=2
+empRate=20
+workingDays=20
 
-Wage=$(( $empRate * $fullTime ));
-echo "Employee Daily Wage is" $Wage
+function empSalary()
+{
+	Range=$((2-1+1))
+	empCheck=$(($((RANDOM%Range))+1))
 
+	case $empCheck  in
+
+	$fullTime)
+		empHours=8;;
+
+	$partTime)
+		empHours=4;;
+
+	*)
+		empHours=0;
+
+	esac
+	dailyWage=$(( $empRate * $empHours))
+	Salary=$(( $dailyWage * $workingDays))
+	echo "Daily Wage Earned by Employee: Rs " $dailyWage
+	echo "Monthly Salary: Rs " $Salary
+}
+empSalary
